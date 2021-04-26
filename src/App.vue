68d25fb14dfacb2196fa8e5d637cdb71e11a1d86
Nick.Yu <nick.yu@hzn.com.tw>
2021-04-26 07:10:46 +0000

    dropdown

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"> </column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/CloumnList.vue'
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'Nick',
+}
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的專欄',
+    description:
+      '這是test1專欄，一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介',
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+  },
+  {
+    id: 2,
+    title: 'test2的專欄',
+    description:
+      '這是test2專欄，一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介',
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+  },
+  {
+    id: 3,
+    title: 'test3的專欄',
+    description:
+      '這是test3專欄，一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介',
+    // avatar:
+    //   'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+  },
+  {
+    id: 4,
+    title: 'test4的專欄',
+    description:
+      '這是test4專欄，一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介一段專欄簡介',
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+  },
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList,
+    GlobalHeader,
+  },
+  setup() {
+    return {
+      list: testData,
+      currentUser,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a href="#" class="navbar-brand">專欄</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">登入</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">註冊</a>
+      </li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <dropdown :title="`你好 ${user.name}`">
+          <dropdown-item
+            ><a class="dropdown-item" href="#">新建文章</a></dropdown-item
+          >
+          <dropdown-item disabled
+            ><a class="dropdown-item" href="#">編輯資料</a></dropdown-item
+          >
+          <dropdown-item
+            ><a class="dropdown-item" href="#">退出登入</a></dropdown-item
+          >
+        </dropdown>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
+
+export interface UserProps {
+  isLogin: boolean
+  name?: string
+  id?: string
+}
+
+export default defineComponent({
+  name: 'GlobalHeader',
+  components: {
+    Dropdown,
+    DropdownItem,
+  },
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true,
+    },
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
